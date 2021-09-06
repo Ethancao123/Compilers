6d1ecd1fde562a143bb864f8d81b7b6cd96e91c3
Ethancao123 <ethan.cao123456@gmail.com>
2021-09-06 16:29:04 -0700

    Fixed project structure

--- JFlexLab/EthanCScanner/src/App.java
+++ JFlexLab/EthanCScanner/src/App.java
@@ -1,11 +1,9 @@
-import scanner.EthanCScanner;
 import java.io.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(new File("JFlexLab/test.txt")));
         EthanCScanner scan = new EthanCScanner(br);
-        boolean eof = false;
         String token = "";
         do
         {
