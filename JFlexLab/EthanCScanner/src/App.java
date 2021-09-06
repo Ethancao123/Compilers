2ed0af4af116d582be3ec570f364b6d42b5c2cf9
Ethancao123 <ethan.cao123456@gmail.com>
2021-09-06 16:11:02 -0700

    EthanCScanner Works

--- JFlexLab/EthanCScanner/src/App.java
+++ JFlexLab/EthanCScanner/src/App.java
@@ -1,12 +1,9 @@
 import scanner.EthanCScanner;
 import java.io.*;
 
-public class ScannerRunner {
-    public static void main(String[] args)
-    {
-        File file = new File("C:\\Users\\ethan\\OneDrive\\Documents\\GitHub\\Compilers\\JFlex Lab\\test.txt");
-        System.out.println(file.exists());
-        BufferedReader br = new BufferedReader(new FileReader(file));
+public class App {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(new File("JFlexLab/test.txt")));
         EthanCScanner scan = new EthanCScanner(br);
         boolean eof = false;
         String token = "";
