d6d046ca6fcf33e1b0f5e38f6e5488364560db18
Ethancao123 <ethan.cao123456@gmail.com>
2021-09-23 14:33:11 -0700

    Created the Parser Project

---
+++ parser/src/Parser.java
@@ -0,0 +1,41 @@
+/**
+ * Parser for a Pascal Compiler
+ * 
+ * @author Ethan Cao
+ * @version 9/23/21
+ */
+public class Parser 
+{
+    private EthanCScannerLab scanner;
+    private String currentToken;
+    
+    /**
+     * Constructor for objects of the Parser Class
+     * @param scan the scanner to request tokens from
+     * @throws ScanErrorException when an illegal character is scanned
+     */
+    public Parser(EthanCScannerLab scan) throws ScanErrorException
+    {
+        scanner = scan;
+        currentToken = scanner.nextToken();
+    }
+
+    /**
+     * Checks if the expected next String is the same as the actual next String 
+     * and consumes one token if they are equal
+     * @param expected the expected next String
+     * @throws IllegalArgumentException if the Strings are not equal
+     * @throws ScanErrorException when an illegal character is scanned
+     */
+    private void eat(String expected) throws IllegalArgumentException, ScanErrorException
+    {
+        if(expected.equals(currentToken))
+        {
+            currentToken = scanner.nextToken();
+        }
+        else
+        {
+            throw new IllegalArgumentException("Expected " + currentToken + " found " + expected);
+        }
+    }
+}
