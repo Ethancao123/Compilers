f5995416381c2ea40bac09acdfb9becec93d7f01
Ethancao123 <ethan.cao123456@gmail.com>
2021-10-25 12:53:36 -0700

    Documented things

--- ast/src/ast/Condition.java
+++ ast/src/ast/Condition.java
@@ -1,10 +1,22 @@
 package ast;
 
-public class Condition {
+/**
+ * Condition Class for a Pascal compiler
+ * @author Ethan Cao
+ * @version 10/25/21 
+ */
+public class Condition 
+{
     String relop;
     Expression exp1;
     Expression exp2;
 
+    /**
+     * Constructor for objects of the Condition class
+     * @param e1 the first expression in the conditional statement
+     * @param r the relative operator of the conditional statement
+     * @param e2 the second expression in the conditional statement
+     */
     public Condition(Expression e1, String r, Expression e2)
     {
         relop = r;
@@ -12,6 +24,11 @@ public class Condition {
         exp2 = e2;
     }
 
+    /**
+     * Evaluates the result of the conditional statement
+     * @param env the environment to evaluate the result in
+     * @return the result of the conditional statement
+     */
     public boolean eval(Environment env)
     {
         int result1 = exp1.eval(env);
--- ast/src/ast/While.java
+++ ast/src/ast/While.java
@@ -1,16 +1,30 @@
 package ast;
 
+/**
+ * While class for a Pascal compiler
+ * @author Ethan Cao
+ * @version 10/25/21
+ */
 public class While extends Statement
 {
     Statement stmt;
     Condition cond;
 
+    /**
+     * Constructor for objects of the While class
+     * @param s the statement to execute in the loop
+     * @param c the condition to execute the loop under
+     */
     public While(Statement s, Condition c)
     {
         stmt = s;
         cond = c;
     }
 
+    /**
+     * Executes the statments while the condition is true
+     * @param env the environment to execute the statements in
+     */
     public void exec(Environment env)
     {
         while(cond.eval(env))
