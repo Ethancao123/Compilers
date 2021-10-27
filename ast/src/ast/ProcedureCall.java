5c296dc021863526df84eb0ee1c1468fc182730d
Ethancao123 <ethan.cao123456@gmail.com>
2021-10-27 10:50:24 -0700

    Added Documentation

--- ast/src/ast/ProcedureCall.java
+++ ast/src/ast/ProcedureCall.java
@@ -1,14 +1,28 @@
 package ast;
 
+/**
+ * Class for procedure calls in a Pascal compiler
+ * @author Ethan Cao
+ * @version 10/27/21
+ */
 public class ProcedureCall extends Expression
 {
-    public String name;
+    private String name;
 
+    /**
+     * Constructor for objects of the ProcedureCall class
+     * @param n the name of the procedure that is called
+     */
     public ProcedureCall(String n)
     {
         name = n;
     }
 
+    /**
+     * Evaluates the procedure call
+     * @param env the environment to evaluate the procedure call in
+     * @return the result of the procedure call
+     */
     public int eval(Environment env)
     {
         env.getProcedure(name).exec(env);
