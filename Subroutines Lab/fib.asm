1a20cf54625bd26356da114eae5c1d84c8752e16
Ethancao123 <ethan.cao123456@gmail.com>
2021-12-31 15:36:50 -0800

    Finished subroutines

--- Subroutines Lab/fib.asm
+++ Subroutines Lab/fib.asm
@@ -8,64 +8,62 @@
     .text 0x00400000
     .globl main
 main:
-    #prompts the user for input
+    #Prompt user
     li $v0, 4
     la $a0, msg1
-    syscall 
+    syscall
+
+    #Read result
     li $v0, 5
     syscall
+
+    #prints zero if the input is zero
+    beq $v0, 0, isZero
+
+    #Call fib
     move $a0, $v0
-    #jumps to the fib subroutine
-    subu $sp, $sp, 4
-    sw $ra, ($sp)
     jal fib
-    lw $ra, ($sp)
-    addu $sp, $sp, 4
-    move $a0, $v0
-    #prints the result
+    move $a1, $v0
+
+    #Print result
     li $v0, 1
+    move $a0, $a1
     syscall
-end:
     li $v0, 10
     syscall
+
+
 fib:
-    #checks if n is less than or equal to one
-    ble $a0, 1, return
+    #store previous results to the stack
+    subu $sp, $sp, 12
+    sw $ra, 8($sp)
+    sw $t0, 4($sp)
+    sw $t1, 0($sp)
     move $t0, $a0
-    #adds n to the stack
-    subu $sp, $sp, 4
-    sw $a0, ($sp)
-    #adds fib(n-1) to the stack
-    sub $a0, $a0, 1
-    subu $sp, $sp, 4
-    sw $ra, ($sp)
+
+    #return 1 if n is less than 2
+    li $v0, 1
+    ble $t0, 2, end
+
+    #call fib(n-1)
+    subu $a0, $t0,  1
     jal fib
-    lw $ra, ($sp)
-    addu $sp, $sp, 4
-    #adds the result to the stack
-    subu $sp, $sp, 4
-    sw $v0, ($sp)
-    #adds fib(n-2) to the stack
+    move $t1, $v0
+
+    #call fib(n-2)
+    subu $a0, $t0, 2
+    jal fib
+    add $v0, $t1, $v0
+
+end:
+    #empty the stack
+    lw $ra, 8($sp)
     lw $t0, 4($sp)
-    sub $a0, $t0, 2
-    subu $sp, $sp, 4
-    sw $ra, ($sp)
-    jal fib
-    lw $ra, ($sp)
-    addu $sp, $sp, 4
-    #adds the result to the stack
-    subu $sp, $sp, 4
-    sw $v0, ($sp)
-    #adds the two fibs together
-    lw $t0, ($sp)
-    addu $sp, $sp, 4
-    lw $t1, ($sp)
-    addu $sp, $sp, 4
-    addu $v0, $t1, $t2
-    #removes n from the stack
-    addu $sp, $sp, 4
+    lw $t1, 0($sp)
+    addu $sp, $sp, 12
     jr $ra
-return:
-    move $v0, $a0
-    jr $ra
-    
+
+isZero:
+    li $v0, 1
+    la $a0, 0
+    syscall
